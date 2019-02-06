da3de48cc50d2746c13ad4dd21f78624d68c4be1
izhar-plainsurf <izhar.plainsurf@gmail.com>
2019-02-06 14:16:09 +0530

    contact form

---
+++ includes/contact_form.php
@@ -0,0 +1,126 @@
+
+
+<section class = "contact py-5">
+    <div class = "container py-sm-3">
+        <h3 class = "heading text-capitalize mb-lg-5 mb-4"> Contact Us - <span>Get In Touch</span> </h3>
+        <div class = "address row">
+            <div class = "col-md-4 address-grid">
+                <div class = "address-info">
+                    <div class = "address-left text-center">
+                        <i class = "far fa-map"></i>
+                    </div>
+                    <div class = "address-right text-center">
+                        <h6 class = "ad-info text-uppercase mb-2">Address</h6>
+                        <p> 2466H 5th Street Parking, King Block, New York City.
+
+                        </p>
+                    </div>
+                </div>
+
+            </div>
+            <div class = "col-md-4 address-grid">
+                <div class = "address-info">
+                    <div class = "address-left text-center">
+                        <i class = "far fa-envelope"></i>
+                    </div>
+                    <div class = "address-right text-center">
+                        <h6 class = "ad-info text-uppercase mb-2">Email</h6>
+                        <a href = "mailto:example@email.com"> mail@example.com</a>
+                        <a href = "mailto:example@email.com"> mail@example.com</a>
+                    </div>
+
+                </div>
+            </div>
+            <div class = "col-md-4 address-grid">
+                <div class = "address-info">
+                    <div class = "address-left text-center">
+                        <i class = "fas fa-mobile-alt"></i>
+                    </div>
+                    <div class = "address-right text-center">
+                        <h6 class = "ad-info text-uppercase mb-2">Phone</h6>
+                        <p>+1 234 567 8901</p>
+                        <p>+1 234 567 8901</p>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="row">
+
+                    <div class="col-lg-6">
+
+                        <form action="/action_page.php">
+                            <div class="form-group">
+                                <label for="email">First Name:</label>
+                                <input type="text" class="form-control" id="email" placeholder="First Name">
+                            </div>
+                            <div class="form-group">
+                                <label for="email">Email:</label>
+                                <input type="email" class="form-control" id="pwd" placeholder="Email">
+                            </div>
+
+
+                        </form> 
+                    </div>
+                    <div class="col-lg-6">
+                        <form action="/action_page.php">
+                            <div class="form-group">
+                                <label>Last Name:</label>
+                                <input type="text" class="form-control" id="email" placeholder="Last Name">
+                            </div>
+                            <div class="form-group">
+                                <label>Mobile no.</label>
+                                <input type="text" class="form-control" id="pwd" placeholder="Mobile no">
+                            </div>
+
+                        </form> 
+                    </div>
+                     
+                </div>
+<label for="comment">Discription:</label>
+<textarea class="form-control" rows="5" id="comment" placeholder="Type here..."></textarea>
+<div class="row">
+    <div class="col-lg-4 py-2">
+        <div class="radio">
+  <label><input type="radio" name="optradio" checked>Freinds</label>
+</div>
+    </div>
+    <div class="col-lg-4 py-2">
+        <div class="radio">
+  <label><input type="radio" name="optradio" checked>Social</label>
+</div>
+    </div>
+    <div class="col-lg-4 py-2">
+        <div class="radio">
+  <label><input type="radio" name="optradio" checked>Other</label>
+</div>
+    </div>
+</div>
+<div class="py-3">
+<button type="button" class="btn btn-success">Submit</button>
+ 
+</div>
+            </div>
+            <div class = "col-lg-6 mt-lg-0 mt-5">
+                <div class = "bg-img">
+                    <div class = "contact-layer">
+                        <h3 class = "mb-3">Dont hesitate to contact us for any kind of information</h3>
+                        <p><i class = "fas mr-2 fa-phone"></i> +12 327 8976 2334</p>
+                    </div>
+                </div>
+            </div> 
+
+
+
+
+
+        </div>
+
+        <div class = "map py-5">
+            <iframe src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d387142.84010033106!2d-74.25819252532891!3d40.70583163828471!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew+York%2C+NY%2C+USA!5e0!3m2!1sen!2sin!4v1475140387172"
+                    style = "border:0"></iframe>
+        </div>
+    </div>
+</section>	
